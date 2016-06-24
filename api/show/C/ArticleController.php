c117a046c3c3055586ab802a623d12bfcbdc2a0b
vimkidgit <1779156598@qq.com>
2016-06-24 15:29:44 +0800

    add info page

--- api/show/C/ArticleController.php
+++ api/show/C/ArticleController.php
@@ -12,10 +12,15 @@ class ArticleController {
 		$mysql = new system\core\db\Mysql();
 		$sql = "select * from vimkid_article where article_id =".$articleId;
 		$result = $mysql->select($sql);
-		$params=$result[0];
-		$Parsedown = new system\plugin\outer\parsedown\Parsedown();
-		$params['html'] =  $Parsedown->text($params['article_content']); # prints: <p>Hello <em>Parsedown</em>!</p>
-		Render::renderTpl('static/detail.html',$params);
+		if(isset($result[0])){
+			$params=$result[0];
+			$Parsedown = new system\plugin\outer\parsedown\Parsedown();
+			$params['html'] =  $Parsedown->text($params['article_content']); # prints: <p>Hello <em>Parsedown</em>!</p>
+			Render::renderTpl('static/detail.html',$params);
+		}else{
+			$params['errorInfo']='访问的文章不存在';
+			Render::renderTpl('static/info.html',$params);
+		}
 	}
 }
 ?>
