9c92545c0275c3ae8834e59147e08ac42726df25
JamesKid <406358964@qq.com>
2016-08-27 23:52:00 +0800

    fix update

--- system/shell/update.php
+++ system/shell/update.php
@@ -1,12 +1,13 @@
-j<?php
+<?php
 $dbh = new PDO('mysql:host=localhost;dbname=vimkid', 'root', '123456');
 $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 $dbh->exec('set names utf8');
 /* 参数列表 */
 $content = file_get_contents('/var/www/article/2_vim最最基本的使用.md');
 $title = "vim最最基本的使用";
-$sql = "update vimkid_article set article_content=".$content." where article_title=".$title;
+$sql = "UPDATE vimkid_article SET article_content='".$content."' where article_title='".$title."'";
 $stmt = $dbh->prepare($sql);
-$stmt->execute($sql);
+#$stmt->execute($sql);
+$stmt->execute();
 print_r($stmt);
 ?>
