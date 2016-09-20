dde04c7916d5c79d86e906c0e0e95acb4cbd96a7
JamesKid <406358964@qq.com>
2016-09-20 23:35:14 +0800

    fix update by id

--- system/shell/update.php
+++ system/shell/update.php
@@ -2,14 +2,12 @@
 $dbh = new PDO('mysql:host=localhost;dbname=vimkid', 'root', '123456');
 $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 $dbh->exec('set names utf8');
+$id = 3;
 /* 参数列表 */
-$content = file_get_contents('/var/www/article/1_vimkid网站发布啦.md');
-//$content = file_get_contents('/var/www/article/3_vim插件之MRU.md');
-$title = "vimkid网站发布啦";
-//$title = "vim插件之MRU";
-$sql = "UPDATE vimkid_article SET article_content='".$content."' where article_title='".$title."'";
+$filename =  exec('ls  /var/www/article | grep '.$id.'_');
+$content = file_get_contents('/var/www/article/'.$filename);
+$sql = "UPDATE vimkid_article SET article_content='".$content."' where article_id=".$id;
 $stmt = $dbh->prepare($sql);
-#$stmt->execute($sql);
 $stmt->execute();
 print_r($stmt);
 ?>
