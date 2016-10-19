75691710a03867d8fc29464ace3ef3b382748d95
vimkidgit <1779156598@qq.com>
2016-10-19 17:39:35 +0800

    fix css and update

--- system/shell/update.php
+++ system/shell/update.php
@@ -2,10 +2,11 @@
 $dbh = new PDO('mysql:host=localhost;dbname=vimkid', 'root', '123456');
 $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 $dbh->exec('set names utf8');
-$id = 1;
+$id = 6;
 /* 参数列表 */
 $filename =  exec('ls  /var/www/article | grep '.$id.'_');
 $content = file_get_contents('/var/www/article/'.$filename);
+$content = addslashes($content);
 $sql = "UPDATE vimkid_article SET article_content='".$content."' where article_id=".$id;
 $stmt = $dbh->prepare($sql);
 $stmt->execute();
