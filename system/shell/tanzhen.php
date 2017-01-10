9121a9b43a292fcdf3bae69db5a65248bd0677e5
vimkidgit <1779156598@qq.com>
2017-01-10 16:54:20 +0800

    add php 探针

---
+++ system/shell/tanzhen.php
@@ -0,0 +1,14 @@
+<?php
+// ================================JamesKid============================== 
+// @author       : JamesKid  
+// @description  : This is a php tanzhen scirpt ,show about :
+//                 1. php server status
+//                 2. mysqlserver status
+//                 3. nginxserver status
+//                 4. system cpu memory brandwidth use
+//
+// @change       :
+// ======================================================================
+
+
+?>
