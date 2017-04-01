ceab968a99b91e099b67de9de85ac6581264d51e
vimkidgit <1779156598@qq.com>
2017-04-01 10:48:25 +0800

    update

--- api/show/C/PageController.php
+++ api/show/C/PageController.php
@@ -6,16 +6,5 @@ class PageController {
 		$params['page'] = new system\plugin\outer\Page\Page($total,$pagesize);
 		Render::renderTpl('static/page.html',$params);
 	}
-	public function edit(){
-		$test = 1;
-		$params['title'] = 'page';
-		$params['content'] = 'page';
-		$params['arr'] = array(
-			'0'=>'helloalkjklj',
-			'1'=>'helloblkkj',
-			'2'=>'helloclkj;kj',
-		);
-		$this->render('indext',$params);
-	}
 }
 ?>
