ba4058040074666dd18a04fee1a3e720e0ae3519
vimkidgit <1779156598@qq.com>
2017-05-02 17:50:00 +0800

    update

--- system/core/PublicCore.php
+++ system/core/PublicCore.php
@@ -7,4 +7,19 @@ class PublicCore {
 		include($_SERVER['DOCUMENT_ROOT'].'/conf/config_'.trim($env).'.php'); //引用配置文件
 		$this->config = $config;  //获取配置
 	}
+
+	public function getAddress($ip){
+		/* 获取表 */
+		$point = strpos($ip,'.');
+		$table = substr($ip,0,$point);
+		$getIpInfo = new system\core\db\Mysql('IP');
+		$sql = "select ip_country,ip_city,ip_province from `".$table."` where inet_aton(ip_begin) < inet_aton('".$ip."') and inet_aton(ip_end)> inet_aton('".$ip."')";
+		$getIpInfo = $getIpInfo->execute($sql);
+		$result =  $getIpInfo->fetchAll(PDO::FETCH_ASSOC);
+		return $result[0];
+	}
+	public function getIpInfo(){
+        $ipInfo = new system\plugin\outer\GetIpInfo\GetIpInfo(); // 获取ip
+        return $ipInfo;
+    }
 }
