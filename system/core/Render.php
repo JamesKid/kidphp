52f18212c3779bc2fd07f665095a27ae9894df68
vimkidgit <1779156598@qq.com>
2017-05-09 15:19:50 +0800

    update

--- system/core/Render.php
+++ system/core/Render.php
@@ -1,8 +1,8 @@
 <?php
 class Render {
-	public static function renderTpl($tpl,$params='',$api='show'){
-		$viewDirectory=$_SERVER['DOCUMENT_ROOT'];
-		$viewDirectory = $viewDirectory.'/api/'.$api.'/V/'.$tpl; // 获取你级目录
-		require_once($viewDirectory);
-	}
+    public static function renderTpl($tpl,$params='',$api='show'){
+        $viewDirectory=$_SERVER['DOCUMENT_ROOT'];
+        $viewDirectory = $viewDirectory.'/api/'.$api.'/V/'.$tpl; // 获取你级目录
+        require_once($viewDirectory);
+    }
 }
