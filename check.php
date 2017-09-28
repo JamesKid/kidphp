da50d626f1953df24ce7e6f7e61d97154daf1296
vimkidgit <1779156598@qq.com>
2017-09-28 16:27:23 +0800

    update

--- check.php
+++ check.php
@@ -1,10 +1,12 @@
 <?php
+// 健康检查
 echo 'v-ok';
 /* 
- * # tips 
- *     可以用ab -kc 500 -n 5000 www.xxx.com/check.php 来检测服务器基础响应能力
- * # 常用命令
+ * # tips (命令行下性能检测)
+ *     ab -kc 500 -n 5000 www.xxx.com/check.php  # 检测服务器基础响应能力
+ *     sh /etc/myconf/shell/status/1_mysql_qps_tps.sh  # 检测mysql服务器响应能力和请求数量
  *     dstat -m    # 查看内存占用
+ *     dstat       # 查看cpu占用
  *
  * 服务器性能列表(每秒响应越大表示性能越好)
  *     1. (vps 512M 20G )
