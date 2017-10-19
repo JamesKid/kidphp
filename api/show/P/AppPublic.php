53c43e2c46b20f2342aa09d1f0328cebb8107d59
vimkidgit <1779156598@qq.com>
2017-10-19 18:33:50 +0800

    update

--- api/show/P/AppPublic.php
+++ api/show/P/AppPublic.php
@@ -5,5 +5,42 @@ class AppPublic{
         $viewDirectory = substr($viewDirectory,0,strlen($viewDirectory)-1); // 获取你级目录
         require_once($viewDirectory.'V/'.ucfirst($tpl).'View.php');
     }
+
+    /* 检查是否来自本域 */
+    public function checkFrom(){
+        if(!isset($_SERVER['HTTP_REFERER'])){
+            return false;
+        }
+        if($_SERVER['HTTP_REFERER'] != $GLOBALS['CONFIG']['DOMAIN']){
+            return false;
+        }
+        return true;
+    }
+
+    /* 安全获取GET,POST */
+    public function getStr($string){
+        $string = $this->public_safe_replace($string);  
+        $string = addslashes($string);  
+        return $string;  
+    }
+
+    /** 
+     * 安全过滤函数 
+     */  
+    public function public_safe_replace($string)  
+    {  
+        $string = str_replace('%20','',$string);  
+        $string = str_replace('%27','',$string);  
+        $string = str_replace('%2527','',$string);  
+        $string = str_replace('*','',$string);  
+        $string = str_replace('"','"',$string);  
+        $string = str_replace("'",'',$string);  
+        $string = str_replace('"','',$string);  
+        $string = str_replace(';','',$string);  
+        $string = str_replace('<','<',$string);  
+        $string = str_replace('>','>',$string);  
+        $string = str_replace("{",'',$string);  
+        $string = str_replace('}','',$string);  
+        return $string;  
+    }  
 }
-?>
---
+++ conf/config_language.php
@@ -0,0 +1,58 @@
+<?php
+$GLOBALS['LANGUAGE'] = array(
+    'nowLanguage' => 'en',  // 当前默认语言
+    'vimCategory'=>array(   // vim 目录
+        'vimuse'=>array(
+            'link'=> '/vimuse',
+            'languageList'=>array(
+                'en'=>'Vim Use',
+                'zh'=>'Vim基本使用',
+            ),
+        ),
+        'vimplugin'=>array(
+            'link'=> '/vimplugin',
+            'languageList'=>array(
+                'en'=>'Vim Plugin',
+                'zh'=>'Vim插件',
+            ),
+        ),
+        'vimmanual'=>array(
+            'link'=> '/vimmanual',
+            'languageList'=>array(
+                'en'=>'Vim Manual Read',
+                'zh'=>'Vim手册解读',
+            ),
+        ),
+        'vimconfig'=>array(
+            'link'=> '/vimconfig',
+            'languageList'=>array(
+                'en'=>'Vim Config',
+                'zh'=>'Vim配置',
+            ),
+        ),
+        'vimlearn'=>array(
+            'link'=> '/vimlearn',
+            'languageList'=>array(
+                'en'=>'Vim Learn Tips',
+                'zh'=>'Vim学习Tips',
+            ),
+        ),
+
+    ),
+    'mainCategory'=>array(   // 主目录
+        'index'=>array(
+            'link'=> '/index.html',
+            'languageList'=>array(
+                'en'=>'Index',
+                'zh'=>'首页',
+            ),
+        ),
+        'Others'=>array(
+            'link'=> '/others',
+            'languageList'=>array(
+                'en'=>'Others',
+                'zh'=>'其他',
+            ),
+        ),
+    ),
+);
