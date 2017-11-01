3f653060c577606a9fe06f041f19f1b185fae616
vimkidgit <1779156598@qq.com>
2017-11-01 23:26:16 +0800

    add clean shell and fix category

--- api/show/S/CategoryService.php
+++ api/show/S/CategoryService.php
@@ -4,11 +4,10 @@ class CategoryService {
     /* 获取目录内容条数 */
     public function getSubListNumber($category){
         $mysql = new system\core\db\Mysql();
-        $sql = "select 
-                count(1)  as number
-            from vimkid_article 
-            where article_status=1 
-                and article_subcategoryname='".$category."'";
+        $sql = "select count(1)  as number
+            from vimkid_article AS a, vimkid_article_info_en AS b
+            where a.article_id = b.article_id and a.article_status=1 
+                and b.article_subcategoryname='".$category."'";
         $result = $mysql->execute($sql);
         $result = $result->fetchAll();
         return $result[0]['number'];
@@ -18,15 +17,15 @@ class CategoryService {
     public function getSubCategoryList($category,$offset,$size){
         $mysql = new system\core\db\Mysql();
         $sql = "select 
-                article_id,
-                article_title,
-                article_seodescription,
-                article_username,
-                article_createtimeymd
-            from vimkid_article 
-            where article_status=1 
-                and article_subcategoryname='".$category."' "."
-            order by article_createtime desc
+                a.article_id,
+                a.article_username,
+                a.article_createtimeymd,
+                b.article_seodescription,
+                b.article_title
+            from vimkid_article AS a, vimkid_article_info_en AS b 
+            where a.article_id = b.article_id and a.article_status=1 
+                and b.article_subcategoryname='".$category."' "."
+            order by a.article_createtime desc
             limit ".$offset.",".$size;
         $result = $mysql->execute($sql);
         $result = $result->fetchAll();
